5219335fc57f9d7dd5b1484f8542c5aca554592a
eunhye han <hellou8363@gmail.com>
2024-04-14 00:29:28 +0900

    Update 성분으로 구분한 아이스크림 총 주문량.sql

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -4,7 +4,8 @@
 -- ------------------------------------------------
 -- 1. first_half 테이블을 기준으로 icecream_info 테이블과 JOIN한다.
 -- 2. 아이스크림 성분 타입 별로 그룹화한다.
--- 3. 총주문량을 기준으로 오름차순한다.
+-- 3. 아이스크립 성분 타입, 총주문량을 출력한다.
+-- 4. 총주문량을 기준으로 오름차순한다.
 SELECT ingredient_type, sum(total_order)
 FROM first_half t1
     JOIN icecream_info t2
