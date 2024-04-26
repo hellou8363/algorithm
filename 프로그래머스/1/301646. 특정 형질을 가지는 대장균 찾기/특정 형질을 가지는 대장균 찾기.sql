c40da3efd5298a9970dd3cb258debfec980fdc15
eunhye han <hellou8363@gmail.com>
2024-04-26 09:50:28 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -6,4 +6,9 @@ SELECT count(*) AS count
 FROM ecoli_data
 WHERE 
     NOT genotype & 2 -- 두 번째 비트가 0인 레코드
-    AND (genotype & 4 OR genotype & 1) -- 네 번째 또는 첫 번쨰 비트가 1인 레코드
+ -- AND (genotype & 4 OR genotype & 1) -- 네 번째 또는 첫 번쨰 비트가 1인 레코드
+    AND genotype & 5 -- 첫 번째, 네 번째 비트가 1인 레코드
+ -- 0 0 0 1 = 1
+ -- 0 1 0 0 = 4
+ -- ------------ OR 연산
+ -- 0 1 0 1 = 5
