9e8f6d158f7a519921230589867401395e13b5db
eunhye han <hellou8363@gmail.com>
2024-05-09 19:13:23 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(number: IntArray): Int {
+        var count = 0
+        for (i in 0 until number.size) {
+            for (j in i + 1 until number.size) {
+                for (k in j + 1 until number.size) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        count++
+                    }
+                }
+            }
+        }
+        return count
+    }
+}
